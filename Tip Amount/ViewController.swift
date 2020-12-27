3610aa2eacc2a62ac333317d49a1391a63ae321b
Luis <lacosta2@hawk.iit.edu>
2020-12-27 18:35:41 -0500

    Initial Commit

---
+++ Tip Amount/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Tip Amount
+//
+//  Created by user184158 on 12/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calc(_ sender: Any) {
+        let bill = 	Double(billField.text!) ?? 0
+        let tipPercent = [0.1, 0.15, 0.20]
+        let	tip = bill * tipPercent[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+}
+
+	
+            	
+		
---
+++ Tip AmountTests/Tip_AmountTests.swift
@@ -0,0 +1,33 @@
+//
+//  Tip_AmountTests.swift
+//  Tip AmountTests
+//
+//  Created by user184158 on 12/27/20.
+//
+
+import XCTest
+@testable import Tip_Amount
+
+class Tip_AmountTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
